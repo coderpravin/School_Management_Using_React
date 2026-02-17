150a46908566a34ad8ebad726afd303345c0c520
coderpravin <impravin00@gmail.com>
2026-02-17 16:44:08 +0530

    update App.js

--- frontend/school/src/App.js
+++ frontend/school/src/App.js
@@ -16,6 +16,41 @@ function App(){
     }
 };
 
+  const deleteSchool = async (id) =>{
+    try{
+      await axios.delete(`http://127.0.0.1:8000/${id}/`);
+
+      setSchools((prev)=> prev.filter((school) => school.id !== id));
+
+    } catch(error){
+      console.error("The error is ", error)
+    }
+  };
+
+  const editSchool = async (id) =>{
+    try{
+
+      const newName = prompt("Enter your School name :")
+      const newCity = prompt("Enter your City name :")
+
+      if (!newName || !newCity) return;
+
+      await axios.put(`http://127.0.0.1:8000/edit/${id}/`,{
+        name : newName,
+        city : newCity
+      });
+
+      //Frontend update UI
+      setSchools((prev) =>
+        prev.map((school) =>
+          school.id === id? {...school, name:newName, city:newCity} : school
+        ));
+
+    }catch(error){
+      console.error("The error is ", error)
+    }
+  };
+
   useEffect(() =>{
     fetchSchools();
   }, []);
@@ -30,6 +65,7 @@ function App(){
             <th>Sr No</th>
             <th>School Name</th>
             <th>School Location</th>
+            <th>Action</th>
           </tr>
         </thead>
 
@@ -39,6 +75,22 @@ function App(){
               <td>{school.id}</td>
               <td>{school.name}</td>
               <td>{school.city}</td>
+              <td>
+                <button onClick={()=> {
+                  const ok = window.confirm(`Delete School id ${school.id}`);
+                  if (ok) deleteSchool(school.id)
+                }
+                }>Delete</button>
+
+                <button onClick={()=> 
+                  editSchool(school.id)
+                }>
+                  Edit
+                </button>
+
+                <button>Add</button>
+
+              </td>
             </tr>
           ))}
         </tbody>
